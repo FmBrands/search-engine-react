f3edc137562fdd3e9c4fa6e8c8000578422d183e
FmBrands <dupegift2002@gmail.com>
2023-07-29 00:09:32 +0100

    Worked on the search-engine and created a new component

--- src/Weather.js
+++ src/Weather.js
@@ -1,17 +1,18 @@
 import React, { useState } from "react";
 import "./Weather.css";
-import Update from "./Update";
+
+import WeatherInfo from "./WeatherInfo";
 
 import "bootstrap/dist/css/bootstrap.css";
 import axios from "axios";
 
 export default function Weather(props) {
-  const [ready, setReady] = useState(false);
-  const [weatherData, setWeatherData] = useState({});
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
 
   function handleResponse(response) {
-    setReady(true);
     setWeatherData({
+      ready: true,
       temperature: Math.round(response.data.main.temp),
       city: response.data.name,
       date: new Date(response.data.dt * 1000),
@@ -21,10 +22,21 @@ export default function Weather(props) {
       iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
     });
   }
-  if (ready) {
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    alert(city);
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
+  if (weatherData.ready) {
     return (
       <div className="weather">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="row">
             <div className=" col-9 ">
               {" "}
@@ -40,47 +52,17 @@ export default function Weather(props) {
                 type="submit"
                 value="Search"
                 className="  btn btn-primary w-100"
+                onChange={handleCityChange}
               />
             </div>
           </div>
         </form>
-        <h1>{weatherData.city}</h1>
-        <ul>
-          <li>
-            <Update date={weatherData.date} />
-          </li>
-          <li className="text-capitalize">{weatherData.description}</li>
-        </ul>
-        <div className="row mt-3 bottom">
-          <div className="col-6">
-            <div className="clearfix">
-              <img
-                src={weatherData.iconUrl}
-                alt={weatherData.description}
-                className="float-left"
-              />
-
-              <span className="float-left ">
-                <span className="temperature float-left ">
-                  {weatherData.temperature}
-                </span>
-                <span className="unit ">℃</span>
-              </span>
-            </div>
-          </div>
-          <div className="col-6">
-            <ul>
-              <li>Humidity : {weatherData.humidity}%</li>
-              <li>Wind : {weatherData.wind} km/hr</li>
-            </ul>
-          </div>
-        </div>
+        <WeatherInfo data={weatherData} />
       </div>
     );
   } else {
     const apiKey = "d3cfc8e21373609a59380c0e9070f5be";
-
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
     return "loading...";
   }
